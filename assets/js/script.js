868b18494046468d683d2b85d013420aedec9d1d
Dredgen Niall <theoriginalbap@gmail.com>
2021-09-23 15:14:51 -0500

    added functionality for task form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,30 @@
-var buttonE1 = document.querySelector("#save-task");
-var tasksToDoE1 = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#task-form"); 
+var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-var createTaskHandler = function(){
-    var listItemE1 = document.createElement("li");
-    listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task.";
-    tasksToDoE1.appendChild(listItemE1);
-};
+var createTaskHandler = function(event) { 
+  event.preventDefault(); 
 
-buttonE1.addEventListener("click", createTaskHandler);
 
+  //gets the values entered in the from field and drop down menu
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+  //create list item
+  var listItemEl = document.createElement("li"); 
+  listItemEl.className = "task-item"; 
+
+  //create div to hold task info and add to list item
+  var taskTnfoEl = document.createElement("div");
+  
+  //give it a class
+  taskTnfoEl.className ="task-info";
+ 
+  //add HTML content to div
+  taskTnfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  listItemEl.appendChild(taskTnfoEl);
+ 
+  // add entire list item to list
+  tasksToDoEl.appendChild(listItemEl); 
+  }; 
+
+  formEl.addEventListener("submit", createTaskHandler);
